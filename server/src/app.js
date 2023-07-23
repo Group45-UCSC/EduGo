e0463411bb13ee831818b89a5a6d8c93fe797528
L.L. Apsara Hansani <apsliyanage99@gmail.com>
2023-07-23 20:12:14 +0530

    registration functionality started

--- server/src/app.js
+++ server/src/app.js
@@ -1,11 +1,14 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
-app.use(express.json);
+//middleware
+app.use(express.json());
+app.use(cors());
 
 //database connection
-const pool = require("../src/dbConnection");
+// const pool = require("../src/dbConnection");
 
 //import Routes
 const adminRouter = require("./routes/adminRoutes");
@@ -14,6 +17,8 @@ const parentRoutes = require("./routes/parentRoutes");
 const supAgentRoutes = require("./routes/supAgentRoutes");
 const vcRoutes = require("./routes/vcRoutes");
 const userRoutes = require("./routes/userRoutes");
+//register and login routes
+const jwtAuthRoutes = require("./routes/jwtAuth");
 
 //define routes to use in app
 app.use("/api/admin", adminRouter);
@@ -22,6 +27,7 @@ app.use("/api/parent", parentRoutes);
 app.use("api/supAgent", supAgentRoutes);
 app.use("/api/vc", vcRoutes);
 app.use("/api/user", userRoutes);
+app.use("/api/auth", jwtAuthRoutes);
 
 //export object
 module.exports = app;
