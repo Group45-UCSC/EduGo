e2c04b87f87c0339cf7b72bbfe5625651e2275c0
L.L. Apsara Hansani <apsliyanage99@gmail.com>
2023-07-25 12:01:35 +0530

    user register server side done

--- server/src/controllers/user/userController.js
+++ server/src/controllers/user/userController.js
@@ -1,4 +1,64 @@
-const query = require("../../models/userModel");
+//db connection
+const pool = require("../../dbConnection");
+// const query = require("../../models/userModel");
+
+//password security
+const bcrypt = require("bcrypt");
+
+//jwtGenerator
+const jwtGenerator = require("../../utils/jwtGenerator");
+
+//user register function
+const register = async (req, res) => {
+  try {
+
+    //1. destructure the req.body(name, email, password)
+
+    const { name, email, password } = req.body;
+    
+
+    //2. check if user exist(if exist then throw error)
+
+    const user = await pool.query("SELECT * FROM users WHERE user_email = $1", [
+      email,
+    ]);
+
+    if(user.rows.length !==0){
+      return res.status(401).send("User already exists");
+    }
+
+
+    //3. Bcrypt the user password
+
+    const saltRound = 10;
+    const salt = await bcrypt.genSalt(saltRound);
+
+    const bcryptPassword = await bcrypt.hash(password, salt);
+
+
+    //4. enter the new user inside our database
+
+    const newUser = await pool.query(
+      "INSERT INTO users (user_name,user_email,user_password) VALUES($1,$2,$3) RETURNING * ",
+      [name, email, bcryptPassword]
+    );
+
+    // res.json(newUser.rows[0]);
+
+
+    //5. generating our jwt token
+
+    const token = jwtGenerator(newUser.rows[0].user_id);
+
+    res.json({ token });
+
+
+
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+};
 
 //login function -> POST method
 const login = async (req, res) => {
@@ -12,4 +72,4 @@ const login = async (req, res) => {
   }
 };
 
-module.exports = { login };
+module.exports = { login, register };
--- server/src/routes/userRoutes.js
+++ server/src/routes/userRoutes.js
@@ -3,10 +3,10 @@ const express = require("express");
 const router = express.Router();
 
 //import functions
-const {login} = require("../controllers/user/userController");
+const { register, login } = require("../controllers/user/userController");
 
 //create routes
+router.route("/register").post(register);
 router.route("/login").post(login);
 
 module.exports = router;
-
