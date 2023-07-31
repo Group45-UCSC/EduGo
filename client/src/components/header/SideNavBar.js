60e6c1c980bdbf855a5b8928ab012654d73bc46c
L.L. Apsara Hansani <apsliyanage99@gmail.com>
2023-07-31 19:01:34 +0530

    side nav bar mistakes corrected

--- client/src/components/header/SideNavBar.js
+++ client/src/components/header/SideNavBar.js
@@ -50,43 +50,89 @@
 
 // export default SideNavBar;
 
+// import React from "react";
+// import { useState } from "react";
+// import ArrowImg from "../../images/arrow.png";
+// import Logo from "../../images/logo.png";
+// import { NavLink } from "react-router-dom";
+
+
+
+
+
+// function SideNavBar({data}) {
+
+//   const [open, setOpen] = useState(true);
+
+//   return (
+//       <div className={`${open? "w-[260px]" : "w-[112px]"} duration-300 h-screen p-5 pt-8 bg-gray`} >
+//         <img src={ArrowImg} alt="icon" className={`absolute cursor-pointer rounded-full -right-3 top-3 w-7 border-2 ${!open && "rotate-180"} `} onClick={() => setOpen(!open)} 
+//         />
+//         <div className="flex gap-x-4 items-center">
+//           <img src={Logo} alt="logo" className={`cursor-pointer duration-300 scale-75 ${!open && "scale-100 mb-7"}` } />
+//         </div>
+
+//         <div className="pt-7">
+//         {data.map((link, index) => (
+          
+//             <NavLink key={index} to={link.path} className={`text-black text-m flex items-center gap-x-7 cursor-pointer p-2 hover:bg-black hover:text-white border-2 border-black rounded-md bg-orange my-10 ${!open && 'p-4 mt-17 pl-7'}`}>
+//             {link.icon}
+//             <span className={`${!open && 'hidden'} origin-left duration-200`}>{link.title}</span>
+            
+//             </NavLink>
+
+//           ))}
+//         </div>
+//       </div>
+//   );
+// }
+
+// export default SideNavBar;  
+
+
+
+
+
+
 import React from "react";
-import { useState } from "react";
 import ArrowImg from "../../images/arrow.png";
 import Logo from "../../images/logo.png";
 import { NavLink } from "react-router-dom";
 
-
-
-
-
-function SideNavBar({data}) {
-
-  const [open, setOpen] = useState(true);
-
+function SideNavBar({ data, open, setOpen }) {
   return (
-    <div>
-      <div className={`${open? "w-[260px]" : "w-[112px]"} duration-300 h-screen p-5 pt-8 bg-gray relative`} >
-        <img src={ArrowImg} alt="icon" className={`absolute cursor-pointer rounded-full -right-3 top-3 w-7 border-2 ${!open && "rotate-180"} `} onClick={() => setOpen(!open)} 
+    <div className={`${open ? "w-[260px]" : "w-[100px]"} duration-300 h-screen p-5 pt-8 bg-gray`}>
+      <img
+        src={ArrowImg}
+        alt="icon"
+        className={`absolute cursor-pointer rounded-full -right-3 top-3 w-7 border-2 ${!open && "rotate-180"
+          }`}
+        onClick={() => setOpen(!open)}
+      />
+      <div className="flex gap-x-4 items-center">
+        <img
+          src={Logo}
+          alt="logo"
+          className={`cursor-pointer duration-300 scale-75 ${!open && "scale-100 mb-7"
+            }`}
         />
-        <div className="flex gap-x-4 items-center">
-          <img src={Logo} alt="logo" className={`cursor-pointer duration-300 scale-75 ${!open && "scale-100 mb-7"}` } />
-        </div>
-
-      <div className="pt-7">
-      {data.map((link, index) => (
-        
-          <NavLink key={index} to={link.path} className={`text-black text-m flex items-center gap-x-7 cursor-pointer p-2 hover:bg-black hover:text-white border-2 border-black rounded-md bg-orange my-10 ${!open && 'p-4 mt-17 pl-7'}`}>
-          {link.icon}
-          <span className={`${!open && 'hidden'} origin-left duration-200`}>{link.title}</span>
-          
-          </NavLink>
-
-        ))}
       </div>
+      <div className="pt-7">
+        {data.map((link, index) => (
+          <NavLink
+            key={index}
+            to={link.path}
+            className={`text-black text-m flex items-center gap-x-7 cursor-pointer p-2 hover:bg-black hover:text-white border-2 border-black rounded-md bg-orange my-10 ${!open && 'p-4 mt-17 pl-5'}`}
+          >
+            {link.icon}
+            <span className={`${!open && 'hidden'} origin-left duration-200`}>
+              {link.title}
+            </span>
+          </NavLink>
+        ))}
       </div>
     </div>
   );
 }
 
-export default SideNavBar;  
+export default SideNavBar;
