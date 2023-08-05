10b85410f04b103e640e1da12e086899bbc0bd7a
L.L. Apsara Hansani <apsliyanage99@gmail.com>
2023-08-05 16:36:37 +0530

    ride page created

--- client/src/views/driver/Ride.js
+++ client/src/views/driver/Ride.js
@@ -4,6 +4,8 @@ import "../driver/Tabs.css";
 import MainLayout from "../../components/layout/MainLayout";
 import { FaBeer } from "react-icons/fa";
 import { NavLink } from "react-router-dom";
+import user from "../../images/user.png";
+import { MdLocationOn } from "react-icons/md";
 
 const sideNavBarLinks = [
   { title: "Dashboard", path: "/driver/dashboard", icon: <FaBeer /> },
@@ -14,79 +16,109 @@ const sideNavBarLinks = [
 ];
 
 function Ride() {
+  const childDetails = [
+    {
+      id: "C10005",
+      name: "R.B.S.Udayanga",
+      schoolName: "Royal Collage",
+    },
+    {
+      id: "C10009",
+      name: "L.L.A. Hansani",
+      schoolName: "Sujatha collage",
+    },
+    {
+      id: "C10011",
+      name: "K.S.T. Gunawardhana ",
+      schoolName: "Royal Collage",
+    },
+    {
+      id: "C10011",
+      name: "A.W.K.S. Jayasiri ",
+      schoolName: "Royal Collage",
+    },
+    {
+      id: "C10005",
+      name: "R.B.S.Udayanga",
+      schoolName: "Royal Collage",
+    },
+    {
+      id: "C10009",
+      name: "L.L.A. Hansani",
+      schoolName: "Sujatha collage",
+    },
+    {
+      id: "C10011",
+      name: "K.S.T. Gunawardhana ",
+      schoolName: "Royal Collage",
+    },
+    {
+      id: "C10011",
+      name: "A.W.K.S. Jayasiri ",
+      schoolName: "Royal Collage",
+    },
+    {
+      id: "C10005",
+      name: "R.B.S.Udayanga",
+      schoolName: "Royal Collage",
+    },
+    {
+      id: "C10009",
+      name: "L.L.A. Hansani",
+      schoolName: "Sujatha collage",
+    },
+    {
+      id: "C10011",
+      name: "K.S.T. Gunawardhana ",
+      schoolName: "Royal Collage",
+    },
+    {
+      id: "C10011",
+      name: "A.W.K.S. Jayasiri ",
+      schoolName: "Royal Collage",
+    },
+  ];
+
+  const schools = [
+    {
+      id: "sch009",
+      schoolName: "Royal Collage",
+    },
+    {
+      id: "sch004",
+      schoolName: "Sujatha collage",
+    },
+    {
+      id: "sch108",
+      schoolName: "Royal Collage",
+    },
+    {
+      id: "sch002",
+      schoolName: "Royal Collage",
+    },
+    {
+      id: "sch041",
+      schoolName: "Royal Collage",
+    },
+    {
+      id: "sch008",
+      schoolName: "Sujatha collage",
+    },
+    {
+      id: "sch010",
+      schoolName: "Royal Collage",
+    },
+    {
+      id: "sch082",
+      schoolName: "Royal Collage",
+    },
+  ];
+
   const [toggleState, setToggleState] = useState(1);
 
   const toggleTab = (index) => {
     setToggleState(index);
   };
-  const data1 = [
-    {
-      id: "001",
-      type: "Van",
-      make: "Nissan",
-      model: "Caravan",
-      contact: "0716548792",
-      departure: "Homagama",
-    },
-    {
-      id: "002",
-      type: "Van",
-      make: "Nissan",
-      model: "Hiace",
-      contact: "0716548792",
-      departure: "Colombo 07",
-    },
-    {
-      id: "003",
-      type: "Bus",
-      make: "Toyota",
-      model: "coster",
-      contact: "0716548792",
-      departure: "Kirulapone",
-    },
-    {
-      id: "004",
-      type: "Bus",
-      make: "Mitshubishi",
-      model: "Rosa",
-      contact: "0716548792",
-      departure: "Gampaha",
-    },
-  ];
-  const data2 = [
-    {
-      id: "001",
-      type: "Van",
-      make: "Mazda",
-      model: "Hiace",
-      contact: "0716548792",
-      departure: "Homagama",
-    },
-    {
-      id: "002",
-      type: "Van",
-      make: "Toyota",
-      model: "Hiace",
-      contact: "0716548792",
-      departure: "Colombo 07",
-    },
-    {
-      id: "003",
-      type: "Van",
-      make: "Nissan",
-      model: "Hiace",
-      contact: "0716548792",
-      departure: "Kirulapone",
-    },
-    {
-      id: "004",
-      type: "Bus",
-      make: "Micro",
-      model: "Kinglong",
-      contact: "0716548792",
-      departure: "Gampaha",
-    },
-  ];
 
   const rideDetails = [
     {
@@ -202,23 +234,164 @@ function Ride() {
     //   </MainLayout>
     // </div>
 
+    // <div>
+    //   <MainLayout data={sideNavBarLinks}>
+    //     <div className="pt-4 px-6 mb-2">
+    //       <h1 className="text-[#5a5c69] text-[28px] mb-3 leading-8 font-normal cursor-pointer">
+    //         Ride Details
+    //       </h1>
+    //       <div className="flex justify-end w-5/6 ml-24 mb-4">
+    //         <NavLink to="/parent/Children/addnewride">
+    //           <button className="flex justify-center w-56 h-10 bg-orange rounded-md cursor-pointer hover:shadow-lg transform hover:scale-[103%] transition duration-300 ease-out">
+    //             <div className="flex mt-2 gap-3 font-semibold">Past Rides</div>
+    //           </button>
+    //         </NavLink>
+    //       </div>
+    //       <div className="grid grid-rows-2 grid-cols-2 sm:grid-cols-1 md:grid-cols-2 gap-2  my-1">
+    //         {/* --------------------Driver Details------------------------ */}
+    //         <div className="h-[250px] bg-slate-200">
+    //           {rideDetails.map((rideDetails) => (
+    //             <div className="h-7 my-5 p-6 mx-10">
+    //               <h1 className="text-base text-black-600 font-bold">
+    //                 <span className=" pr-20 text-[#5a5c69]">Ride Id :</span>{" "}
+    //                 {rideDetails.id} <br></br>
+    //                 <span className=" pr-24 text-[#5a5c69]">Type : </span>
+    //                 {rideDetails.type} <br></br>
+    //                 <span className="  pr-8 text-[#5a5c69]">
+    //                   Shift 01 Starts :
+    //                 </span>
+    //                 {rideDetails.startLocation1} at {rideDetails.startTime1}{" "}
+    //                 <br></br>
+    //                 <span className=" pr-8 text-[#5a5c69]">
+    //                   Shift 02 Starts :
+    //                 </span>
+    //                 {rideDetails.startLocation2} at {rideDetails.startTime2}
+    //                 <br></br>
+    //                 <span className=" pr-20 text-[#5a5c69]">PayRate:</span>{" "}
+    //                 {rideDetails.paymentRate}
+    //                 <br></br>
+    //                 <span className=" pr-20 text-[#5a5c69]">
+    //                   Distance:
+    //                 </span>{" "}
+    //                 {rideDetails.distance}
+    //               </h1>
+    //             </div>
+    //           ))}
+    //         </div>
+    //         {/* -------------------------children details------------------------- */}
+    //         <div className="rounded-md row-span-2 h-screen overflow-y-auto">
+    //           <div className=" grid gap-4 px-4 h-screen">
+    //             <div className=" bg-slate-300 h-8 flex mt-0 justify-center">
+    //               <h1 className=" text-xl font-bold">
+    //                 Number of Childrens : 12
+    //               </h1>
+    //             </div>
+    //             <div className="flex flex-col gap-4">
+    //               {childDetails.map((child, index) => (
+    //                 <div
+    //                   key={index}
+    //                   className="h-[60px] w-[100%] rounded-[8px] bg-slate-200 border-l-[4px] border-orange flex items-center justify-between px-[30px] cursor-pointer hover:shadow-lg transform hover:scale-[103%] transition duration-300 ease-out"
+    //                 >
+    //                   <div className="">
+    //                     <img
+    //                       src={user}
+    //                       alt="user"
+    //                       className="bg-slate-300 w-10 cursor-pointer rounded-full p-1"
+    //                     />
+    //                   </div>
+    //                   <div className="w-1/3">
+    //                     <div className="leading-4">
+    //                       <div className="flex gap-1">
+    //                         <div className="text-[12px] font-semibold">
+    //                           {child.id}
+    //                         </div>
+    //                       </div>
+    //                       <div className="flex gap-1">
+    //                         <div className="text-[12px] font-semibold">
+    //                           {child.name}
+    //                         </div>
+    //                       </div>
+    //                       <div className="flex gap-1">
+    //                         <div className="text-[12px] font-semibold">
+    //                           {child.schoolName}
+    //                         </div>
+    //                       </div>
+    //                     </div>
+    //                   </div>
+    //                   <div className=" flex gap-5">
+    //                     {/*---------------------Buttons-----------------------------*/}
+    //                     <div className="">
+    //                       <NavLink to="/parent/children/childlocation">
+    //                         <button className="flex justify-center  w-16 h-10  bg-orange hover:bg-[#b3913b] rounded-md cursor-pointer">
+    //                           <div className="flex mt-2 gap-3 font-semibold">
+    //                             <MdLocationOn className=" text-[25px]" />
+    //                           </div>
+    //                         </button>
+    //                       </NavLink>
+    //                     </div>
+    //                   </div>
+    //                 </div>
+    //               ))}
+    //             </div>
+    //           </div>
+    //         </div>
+    //         <div className=" h-[500px] flex flex-col items-center justify-center">
+    //           <div className="w-full rounded-md row-span-2 overflow-y-auto">
+    //             <div className=" grid gap-1 p-4 h-screen">
+    //               <div className=" bg-slate-300 h-8 flex mt-0 border justify-center">
+    //                 <h1 className=" text-xl font-bold">Reaching Schools : 8</h1>
+    //               </div>
+    //               <div className="flex flex-col gap-4">
+    //                 {schools.map((school, index) => (
+    //                   <div
+    //                     key={index}
+    //                     className="h-[60px] w-[100%] rounded-[8px] bg-slate-200 border-l-[4px] border-orange flex items-center justify-between px-[30px] cursor-pointer hover:shadow-lg transform hover:scale-[103%] transition duration-300 ease-out"
+    //                   >
+    //                     <div className="w-1/3">
+    //                       <div className="leading-4">
+    //                         <div className="flex gap-1">
+    //                           <div className="text-[12px] font-semibold">
+    //                             {school.id}
+    //                           </div>
+    //                         </div>
+    //                         <div className="flex gap-1">
+    //                           <div className="text-[12px] font-semibold">
+    //                             {school.schoolName}
+    //                           </div>
+    //                         </div>
+    //                       </div>
+    //                     </div>
+    //                   </div>
+    //                 ))}
+    //               </div>
+    //             </div>
+    //           </div>
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </MainLayout>
+    // </div>
+
     <div>
       <MainLayout data={sideNavBarLinks}>
-        <div className="pt-4 px-6 mb-2">
+        <div>
           <h1 className="text-[#5a5c69] text-[28px] mb-3 leading-8 font-normal cursor-pointer">
             Ride Details
           </h1>
-          <div className="flex justify-end w-5/6 ml-24 mb-4">
+          <div className="flex justify-end w-full mb-4">
             <NavLink to="/parent/Children/addnewride">
               <button className="flex justify-center w-56 h-10 bg-orange rounded-md cursor-pointer hover:shadow-lg transform hover:scale-[103%] transition duration-300 ease-out">
                 <div className="flex mt-2 gap-3 font-semibold">Past Rides</div>
               </button>
             </NavLink>
           </div>
-          <div className="grid grid-rows-2 grid-cols-2 sm:grid-cols-1 md:grid-cols-2 gap-2  my-1">
-            {/* --------------------Driver Details------------------------ */}
-            <div className="h-[250px] bg-slate-200">
-              {rideDetails.map((rideDetails) => (
+          {/*  */}
+          <div className="h-screen grid grid-cols-2">
+            {/* left */}
+            <div className=" grid grid-rows-6">
+              {/* upper row */}
+              <div className="row-span-2 mb-5 rounded-lg bg-slate-300">
+               {rideDetails.map((rideDetails) => (
                 <div className="h-7 my-5 p-6 mx-10">
                   <h1 className="text-base text-black-600 font-bold">
                     <span className=" pr-20 text-[#5a5c69]">Ride Id :</span>{" "}
@@ -235,9 +408,7 @@ function Ride() {
                     </span>
                     {rideDetails.startLocation2} at {rideDetails.startTime2}
                     <br></br>
-                    <span className=" pr-20 text-[#5a5c69]">
-                      PayRate:
-                    </span>{" "}
+                    <span className=" pr-20 text-[#5a5c69]">PayRate:</span>{" "}
                     {rideDetails.paymentRate}
                     <br></br>
                     <span className=" pr-20 text-[#5a5c69]">
@@ -247,25 +418,99 @@ function Ride() {
                   </h1>
                 </div>
               ))}
-            </div>
-            {/* -------------------------Children details------------------------- */}
-            <div className="bg-slate-200 rounded-md h-[500px] row-span-2">
-              <div className=" grid gap-4 p-4 ">
-                <div className=" bg-slate-300 h-8 flex justify-center">
-                  <h1 className=" text-xl font-bold">
-                    Number of Childrens : 12
-                  </h1>
-                </div>
-                
-                <div className="bg-slate-300 h-16">
-                  
+              </div>
+{/* below row */}
+              <div className="row-span-4 mt-5 overflow-y-auto p-5">
+              <div className=" bg-slate-300 h-8 flex mt-0 mb-4 justify-center">
+                    <h1 className=" text-xl font-bold"> 
+                      Reaching Schools : 8
+                    </h1>
+                  </div>
+                <div className="flex flex-col h-12 gap-4">
+                  {schools.map((school, index) => (
+                    <div
+                      key={index}
+                      className="w-[100%] rounded-[8px] bg-slate-200 border-l-[4px] border-orange flex items-center justify-between px-[30px] cursor-pointer hover:shadow-lg transform hover:scale-[103%] transition duration-300 ease-out"
+                    >
+                      <div className=" px-5 flex gap-1 w-1/3 leading-4">
+                        <div className="text-[12px] font-semibold">
+                          {school.id}
+                        </div>
+                      </div>
+                      <div className="flex gap-1 w-1/3 leading-4">
+                        <div className="text-[12px] font-semibold">
+                          {school.schoolName}
+                        </div>
+                      </div>
+                      <div className="flex gap-1 w-1/3 leading-4">
+                        <NavLink to="/parent/children/childlocation">
+                          <button className="flex justify-center m-2  w-8 h-8  bg-orange hover:bg-[#b3913b] rounded-md cursor-pointer">
+                            <div className="flex mt-2 gap-3 font-semibold">
+                              <MdLocationOn className=" text-[20px]" />
+                            </div>
+                          </button>
+                        </NavLink>
+                      </div>
+                    </div>
+                  ))}
                 </div>
               </div>
             </div>
-            <div className=" h-[240px] flex flex-col items-center justify-center">
-              <div className="w-full rounded-md bg-slate-200 h-[240px] flex">
-                <div className=" w-1/2 mt-10 mx-10  my-4">
-                  <div></div>
+            {/* right */}
+            <div className=" px-5">
+              <div className="rounded-md row-span-2 h-screen overflow-y-auto">
+                <div className=" grid gap-4 px-4 h-screen">
+                  <div className=" bg-slate-300 h-8 flex mt-0 justify-center">
+                    <h1 className=" text-xl font-bold">
+                      Number of Childrens : 12
+                    </h1>
+                  </div>
+                  <div className="flex flex-col gap-4">
+                    {childDetails.map((child, index) => (
+                      <div
+                        key={index}
+                        className="h-[60px] w-[100%] rounded-[8px] bg-slate-200 border-l-[4px] border-orange flex items-center justify-between px-[30px] cursor-pointer hover:shadow-lg transform hover:scale-[103%] transition duration-300 ease-out"
+                      >
+                        <div className="">
+                          <img
+                            src={user}
+                            alt="user"
+                            className="bg-slate-300 w-10 cursor-pointer rounded-full p-1"
+                          />
+                        </div>
+                        {/* <div className="w-1/3"> */}
+                        {/* <div className="leading-4"> */}
+                        <div className=" px-5 flex gap-1 w-1/3 leading-4">
+                          <div className="text-[12px] font-semibold">
+                            {child.id}
+                          </div>
+                        </div>
+                        <div className="flex gap-1 w-1/3 leading-4">
+                          <div className="text-[12px] font-semibold">
+                            {child.name}
+                          </div>
+                        </div>
+                        <div className="flex gap-1 w-1/3 leading-4">
+                          <div className="text-[12px] font-semibold">
+                            {child.schoolName}
+                          </div>
+                          {/* </div> */}
+                          {/* </div> */}
+                        </div>
+                        {/* <div className=" flex gap-5">
+                          <div className="">
+                            <NavLink to="/parent/children/childlocation">
+                              <button className="flex justify-center  w-16 h-10  bg-orange hover:bg-[#b3913b] rounded-md cursor-pointer">
+                                <div className="flex mt-2 gap-3 font-semibold">
+                                  <MdLocationOn className=" text-[25px]" />
+                                </div>
+                              </button>
+                            </NavLink>
+                          </div>
+                        </div> */}
+                      </div>
+                    ))}
+                  </div>
                 </div>
               </div>
             </div>
@@ -277,14 +522,3 @@ function Ride() {
 }
 
 export default Ride;
-<div className="bg-slate-300 h-40">
-                  <h1 className=" text-lg mt-4 font-semibold ml-4">
-                    Reachig Schools :
-                  </h1>
-                  <ul className=" ml-4">
-                    <li>Ananda College</li>
-                    <li>Mahamaya Balika College</li>
-                    <li>C.W.W. Kannangara College</li>
-                    <li>Ananda Balika College</li>
-                  </ul>
-                </div>
