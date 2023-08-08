0e65ed23d66d2be5b19c69d854e74d2db4d7fe20
Kawya Sandamini <2020cs085@stu.ucsc.cmb.ac.lk>
2023-08-08 22:00:57 +0530

    Update ride page

--- mobile_apps/driver_app/lib/pages/ride_page.dart
+++ mobile_apps/driver_app/lib/pages/ride_page.dart
@@ -7,28 +7,73 @@ class RidePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Navbar(
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Support Page',
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Text(
+              'Ride Page',
               style: TextStyle(
                 fontSize: 24,
                 fontWeight: FontWeight.bold,
               ),
             ),
-            SizedBox(height: 20),
-            Text(
-              'Welcome to the Support Page.',
-              style: TextStyle(
-                fontSize: 16,
+          ),
+          Expanded(
+            flex: 2,
+            child: Container(
+              padding: EdgeInsets.all(20),
+              height: 30, 
+              color: Colors.blue, // Placeholder color
+              child: Center(
+                child: Text(
+                  'Map',
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.white,
+                  ),
+                ),
               ),
             ),
-            SizedBox(height: 20),
-            // Add your financial related content here
-          ],
-        ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(20.0),
+            child: Text(
+              'Ongoing Rides',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 3,
+            child: SingleChildScrollView(
+              child: DataTable(
+                columns: const <DataColumn>[
+                  DataColumn(label: Text('Student')),
+                  DataColumn(label: Text('Destination Address')),
+                ],
+                rows: const <DataRow>[
+                  DataRow(cells: <DataCell>[
+                    DataCell(Text('Nilmi Disanayaka')),
+                    DataCell(Text('No. 12, Highway Road, Colombo 05')),
+                  ]),
+                  DataRow(cells: <DataCell>[
+                    DataCell(Text('Kawya Sandamini')),
+                    DataCell(Text('No. 12, Highway Road, Colombo 05')),
+                  ]),
+                  DataRow(cells: <DataCell>[
+                    DataCell(Text('Kavindu Tharaka')),
+                    DataCell(Text('No. 12, Highway Road, Colombo 05')),
+                  ]),
+                  
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
