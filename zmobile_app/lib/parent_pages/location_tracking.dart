268e1e3885c920a4788b01e249395b19e63f0537
Kawya Sandamini <2020cs085@stu.ucsc.cmb.ac.lk>
2023-08-09 16:06:32 +0530

    Update location_tracking.dart

--- zmobile_app/lib/parent_pages/location_tracking.dart
+++ zmobile_app/lib/parent_pages/location_tracking.dart
@@ -1,35 +1,94 @@
-// ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors, prefer_const_literals_to_create_immutables
+// ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors, prefer_const_literals_to_create_immutables, non_constant_identifier_names, prefer_const_constructors_in_immutables
 
 import 'package:flutter/material.dart';
 import './navbar.dart';
+// import 'package:google_maps_flutter/google_maps_flutter.dart'; // Import the Google Maps package
 
 class LocationPage extends StatelessWidget {
-   @override
+  @override
   Widget build(BuildContext context) {
     return Navbar(
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Location Page',
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Text(
+              'Locations',
               style: TextStyle(
                 fontSize: 24,
                 fontWeight: FontWeight.bold,
               ),
             ),
-            SizedBox(height: 20),
-            Text(
-              'Welcome to the Location tracking Page.',
-              style: TextStyle(
-                fontSize: 16,
+          ),
+          SizedBox(height: 20),
+          Column(  
+            children: [
+              _buildCard(context, 'Child 1'),
+              _buildCard(context, 'Child 2'),
+              _buildCard(context, 'Child 3'),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildCard(BuildContext context, String locationName) {
+    double cardWidth = MediaQuery.of(context).size.width - 15; // Subtract padding
+    return GestureDetector(
+      onTap: () {
+        // Navigate to the map page when a card is clicked
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => MapPage(locationName: locationName),
+          ),
+        );
+      },
+      child: Card(
+        elevation: 4,
+        child: Container(
+          width: cardWidth,
+          height: cardWidth * 0.4,
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Text(
+                locationName,
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-            SizedBox(height: 20),
-            // Add your financial related content here
-          ],
+              SizedBox(height: 8),
+              Text('Tap to view on location'),
+            ],
+          ),
         ),
       ),
     );
   }
+
+
+}
+
+class MapPage extends StatelessWidget {
+  final String locationName;
+
+  MapPage({required this.locationName});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Map: $locationName'), backgroundColor: Color(0xFF999999)),
+      body: GoogleMap(
+        // Implement Google Maps here
+        // You need to provide your Google Maps API key and configure the map
+        // with markers, camera position, etc.
+      ),
+    );
+  }
+}
+
+GoogleMap() {
 }
