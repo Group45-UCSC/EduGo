574051e4a477c8c4b42e53b9ddb4a19a7d8ccfc2
L.L. Apsara Hansani <apsliyanage99@gmail.com>
2023-08-10 13:22:03 +0530

    finance page completed

--- client/src/views/driver/Finance.js
+++ client/src/views/driver/Finance.js
@@ -1,10 +1,24 @@
 import React from "react";
 import MainLayout from "../../components/layout/MainLayout";
-import { AiOutlineFileAdd, AiFillCar } from "react-icons/ai";
-import { BarChart, Bar, XAxis, YAxis, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { AiFillCar } from "react-icons/ai";
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
 // import { Cell, CartesianGrid } from 'recharts';
-import { AiFillDashboard } from "react-icons/ai"
-import { MdPayments, MdSupportAgent, MdOutlineRateReview } from "react-icons/md";
+import { AiFillDashboard } from "react-icons/ai";
+import {
+  MdPayments,
+  MdSupportAgent,
+  MdOutlineRateReview,
+} from "react-icons/md";
+import { NavLink } from "react-router-dom";
+import { useState } from "react";
 
 const data = [
   {
@@ -56,83 +70,168 @@ const tabeldata = [
     name: "pId005",
     payment: 35000,
     date: "2023/1/12",
-    period: "2022 Nov/Dec"
+    period: "2022 Nov/Dec",
   },
   {
     name: "pId018",
     payment: 46000,
     date: "2023/3/12",
-    period: "2023 Jan/Feb"
+    period: "2023 Jan/Feb",
   },
   {
     name: "pId025",
     payment: 29500,
     date: "2023/4/12",
-    period: "2023 March"
+    period: "2023 March",
   },
   {
     name: "pId032",
     payment: 23500,
     date: "2023/6/12",
-    period: "2023 April/May"
+    period: "2023 April/May",
   },
   {
     name: "pId041",
     payment: 24100,
     date: "2023/8/6",
-    period: "2023 June"
+    period: "2023 June",
   },
 ];
+
+const childDetails = [
+  {
+    id: "C10005",
+    name: "R.B.S.Udayanga",
+    schoolName: "Royal Collage",
+    paymentStatus: "Paid",
+  },
+  {
+    id: "C10009",
+    name: "L.L.A. Hansani",
+    schoolName: "Sujatha collage",
+    paymentStatus: "Paid",
+  },
+  {
+    id: "C10011",
+    name: "K.S.T. Gunawardhana ",
+    schoolName: "Royal Collage",
+    paymentStatus: "Pending",
+  },
+  {
+    id: "C10011",
+    name: "A.W.K.S. Jayasiri ",
+    schoolName: "Royal Collage",
+    paymentStatus: "Not Paid",
+  },
+  {
+    id: "C10005",
+    name: "R.B.S.Udayanga",
+    schoolName: "Royal Collage",
+    paymentStatus: "paid",
+  },
+  {
+    id: "C10009",
+    name: "L.L.A. Hansani",
+    schoolName: "Sujatha collage",
+    paymentStatus: "Not Paid",
+  },
+  {
+    id: "C10011",
+    name: "K.S.T. Gunawardhana ",
+    schoolName: "Royal Collage",
+    paymentStatus: "Paid",
+  },
+  {
+    id: "C10011",
+    name: "A.W.K.S. Jayasiri ",
+    schoolName: "Royal Collage",
+    paymentStatus: "Pending",
+  },
+  {
+    id: "C10005",
+    name: "R.B.S.Udayanga",
+    schoolName: "Royal Collage",
+    paymentStatus: "Not Paid",
+  },
+  {
+    id: "C10009",
+    name: "L.L.A. Hansani",
+    schoolName: "Sujatha collage",
+    paymentStatus: "Pending",
+  },
+  {
+    id: "C10011",
+    name: "K.S.T. Gunawardhana ",
+    schoolName: "Royal Collage",
+    paymentStatus: "Paid",
+  },
+  {
+    id: "C10011",
+    name: "A.W.K.S. Jayasiri ",
+    schoolName: "Royal Collage",
+    paymentStatus: "Pending",
+  },
+];
+
 const sideNavBarLinks = [
   { title: "Dashboard", path: "/driver/dashboard", icon: <AiFillDashboard /> },
   { title: "School Ride", path: "/driver/ride", icon: <AiFillCar /> },
   { title: "Finance", path: "/driver/finance", icon: <MdPayments /> },
   { title: "Support", path: "/driver/support", icon: <MdSupportAgent /> },
-  { title: "Feedback", path: "/driver/feedback", icon: <MdOutlineRateReview /> },
+  {
+    title: "Feedback",
+    path: "/driver/feedback",
+    icon: <MdOutlineRateReview />,
+  },
 ];
 function Finance() {
+  const [filterValue, setFilterValue] = useState("All");
+
+  const filteredChildren =
+    filterValue === "All"
+      ? childDetails
+      : childDetails.filter((child) => child.paymentStatus === filterValue);
+
   return (
     <div>
       <MainLayout data={sideNavBarLinks}>
-        <div className="pt-[25px] px-[25px] ">
-          <h1 className="text-[#5a5c69] text-[28px] leading-8 font-normal cursor-pointer">
-            My Income
-          </h1>
-          <div className="flex justify-end w-5/6 ml-44 mb-4">
-              <button
-                className="flex justify-center w-56 h-10 bg-orange rounded-md cursor-pointer hover:shadow-lg transform hover:scale-[103%] transition duration-300 ease-out"
-                // onClick={() => {
-                //   setModalOpen(true);
-                // }}
-              >
-                <div className="flex mt-2 gap-3 font-semibold">
-                  <AiOutlineFileAdd className="text-[25px]" />
-                  Add Payment
-                </div>
-              </button>
-            </div>
-          <div className="grid grid-cols-2 grid-rows-2 gap-6 mt-3">
-            <div className=" h-[200px]">
-              {/* ----------Income box-------------- */}
-              <div className=" w-full h-full rounded-[8px] bg-slate-200 border-l-[4px] border-orange flex items-center justify-between px-[30px] cursor-pointer hover:shadow-lg transform hover:scale-[103%] transition duration-300 ease-out">
+        <h1 className="text-[#5a5c69] text-[28px] leading-8 font-normal cursor-pointer">
+          My Income
+        </h1>
+        <div className="flex justify-end w-full mb-4">
+          <NavLink to="/driver/ride/riderequests">
+            <button className="flex justify-center w-56 h-10 mr-12 bg-orange rounded-md cursor-pointer hover:shadow-lg transform hover:scale-[103%] transition duration-300 ease-out">
+              <div className="flex mt-2 gap-3 font-semibold">Collected Ride Payments</div>
+            </button>
+          </NavLink>
+        </div>
+        {/* full box */}
+        <div className="h-screen grid gap-3 grid-cols-5 mb-10">
+          {/* left column */}
+          <div className="col-span-3 grid grid-cols-2 grid-rows-2 gap-6 mt-3">
+            {/* <div className="w-full grid grid-cols-3"> */}
+            {/*Income box*/}
+
+            <div className="w-[70%] ml-0 h-[200px]">
+              <div className=" h-full rounded-[8px] bg-slate-200 border-l-[4px] border-orange flex items-center justify-between px-[30px] cursor-pointer hover:shadow-lg transform hover:scale-[103%] transition duration-300 ease-out">
                 <div className=" h-[150px] w-full">
                   <h1 className="text-[24px] leading-[24px] font-semibold text-[#5a5c69] mt-[20px] mb-8">
                     Last Month Income
                   </h1>
                   <div className=" flex justify-center gap-32">
                     <div>
-                      <h2 className=" text-orange text-[38px] leading-[17px] font-bold mb-3">
-                        LKR 3400
+                      <h2 className=" text-orange text-[28px] leading-[17px] ml-[-14px] font-bold mb-3">
+                        LKR 34000
                       </h2>
-
                     </div>
-
                   </div>
                 </div>
               </div>
             </div>
-            <div className=" h-[200px]">
-              {/* chart */}
+            {/* end of Income box*/}
+            {/* chart */}
+
+            <div className="w-[130%] ml-[-120px] h-[200px]">
               <ResponsiveContainer width="100%" height="100%">
                 <BarChart width={150} height={40} data={data}>
                   <XAxis dataKey="name" />
@@ -140,27 +239,58 @@ function Finance() {
                   <Tooltip />
                   <Legend />
                   <Bar dataKey="income" fill=" rgb(51 65 85)" />
-
                 </BarChart>
               </ResponsiveContainer>
+              {/* </div> */}
+              {/* end of chart */}
             </div>
+
+            {/* table */}
             <div className=" col-span-2 h-[400px] mt-[-100px] mb-4">
               <div>
-                <div className='float-right '>
-                  <form action=''>
-                    <input type="text" placeholder='Search..' className=' mt-1 overflow-auto w-40 mr-32  border border-slate-400 pl-2 rounded-md'>
-                    </input>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-10 stroke-slate-500 absolute -mt-8 ml-32 hover:cursor-pointer">
-                      <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+                <div className="float-right ">
+                  <form action="">
+                    <input
+                      type="text"
+                      placeholder="Search.."
+                      className=" mt-1 overflow-auto w-40 mr-32  border border-slate-400 pl-2 rounded-md"
+                    ></input>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke-width="1.5"
+                      stroke="currentColor"
+                      class="w-5 h-10 stroke-slate-500 absolute -mt-8 ml-32 hover:cursor-pointer"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+                      />
                     </svg>
                   </form>
                 </div>
-                <div className='float-right '>
-                  <form action=''>
-                    <input type="text" placeholder='Filter here' className=' mt-1 overflow-auto w-40 mr-8  border border-slate-400 pl-2 rounded-md '>
-                    </input>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-10 stroke-slate-500 absolute -mt-8 ml-32 hover:cursor-pointer">
-                      <path stroke-linecap="round" stroke-linejoin="round" d="M12 3c2.755 0 5.455.232 8.083.678.533.09.917.556.917 1.096v1.044a2.25 2.25 0 01-.659 1.591l-5.432 5.432a2.25 2.25 0 00-.659 1.591v2.927a2.25 2.25 0 01-1.244 2.013L9.75 21v-6.568a2.25 2.25 0 00-.659-1.591L3.659 7.409A2.25 2.25 0 013 5.818V4.774c0-.54.384-1.006.917-1.096A48.32 48.32 0 0112 3z  " />
+                <div className="float-right ">
+                  <form action="">
+                    <input
+                      type="text"
+                      placeholder="Filter here"
+                      className=" mt-1 overflow-auto w-40 mr-8  border border-slate-400 pl-2 rounded-md "
+                    ></input>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke-width="1.5"
+                      stroke="currentColor"
+                      class="w-5 h-10 stroke-slate-500 absolute -mt-8 ml-32 hover:cursor-pointer"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M12 3c2.755 0 5.455.232 8.083.678.533.09.917.556.917 1.096v1.044a2.25 2.25 0 01-.659 1.591l-5.432 5.432a2.25 2.25 0 00-.659 1.591v2.927a2.25 2.25 0 01-1.244 2.013L9.75 21v-6.568a2.25 2.25 0 00-.659-1.591L3.659 7.409A2.25 2.25 0 013 5.818V4.774c0-.54.384-1.006.917-1.096A48.32 48.32 0 0112 3z  "
+                      />
                     </svg>
                   </form>
                 </div>
@@ -171,7 +301,6 @@ function Finance() {
                       <th className="px-3.5 w-30">Amount</th>
                       <th className="px-3.5 w-30">Date</th>
                       <th className="px-3.5 w-30">period</th>
-
                     </tr>
                   </thead>
 
@@ -185,7 +314,6 @@ function Finance() {
                         <td className="text-center">{item.payment}</td>
                         <td className="text-center">{item.date}</td>
                         <td className="text-center">{item.period}</td>
-
                       </tr>
                     ))}
                   </tbody>
@@ -193,8 +321,97 @@ function Finance() {
               </div>
             </div>
           </div>
-        </div>
+          {/* left column */}
+          {/* right column */}
+          <div className="col-span-2 rounded-md h-[550px] ">
+            <div className=" mt-8">
+              <div className="h-8 mb-3 flex justify-center">
+                <h1 className="text-xl font-bold ">Payment Details - July</h1>
+              </div>
+              <div className="mb-1 mt-1 flex justify-end mr-5">
+                <select
+                  className="px-2 py-[2px] rounded-md border border-gray"
+                  value={filterValue}
+                  onChange={(e) => setFilterValue(e.target.value)}
+                >
+                  <option value="All">All</option>
+                  <option value="Paid">Paid</option>
+                  <option value="Pending">Pending</option>
+                  <option value="Not Paid">Not Paid</option>
+                </select>
+              </div>
+              <div className=" px-3 mx-2 rounded-md h-[660px] overflow-y-auto">
+                {filteredChildren.map((child, index) => (
+                  <div key={index}>
+                    {(() => {
+                      switch (child.paymentStatus) {
+                        case "Paid":
+                          return (
+                            <div className="rounded-[8px] bg-[#c8eac8] mb-3 mt-3  border-[1px] border-[#389438]  items-center justify-between px-[30px] py-1 cursor-pointer hover:shadow-lg transform hover:scale-[101%] transition duration-300 ease-out">
+                              <div className="flex  justify-between w-full mb-1">
+                                <div className="flex  justify-start gap-2 ">
+                                  <h1 className="mt-1 text-sm">{child.name}</h1>
+                                </div>
+                                <div className="flex  justify-end gap-2 ">
+                                  <h1 className="mt-1 text-[#428a42]">
+                                    {child.paymentStatus}
+                                  </h1>
+                                </div>
+                              </div>
+                              <div className="text-slate-600 text-xs ">
+                                {child.id}
+                              </div>
+                            </div>
+                          );
+                        case "Pending":
+                          return (
+                            <div className="rounded-[8px] bg-[#fff2cc] mb-3 mt-3  border-[1px] border-orange  items-center justify-between px-[30px] py-1 cursor-pointer hover:shadow-lg transform hover:scale-[101%] transition duration-300 ease-out">
+                              <div className="flex  justify-between w-full mb-1">
+                                <div className="flex  justify-start gap-2 ">
+                                  <h1 className="mt-1 text-sm">{child.name}</h1>
+                                </div>
+                                <div className="flex  justify-end gap-2 ">
+                                  <h1 className="mt-1 text-[#e67300]">
+                                    {child.paymentStatus}
+                                  </h1>
+                                </div>
+                              </div>
+                              <div className="text-slate-600 text-xs">
+                                {child.id}
+                              </div>
+                            </div>
+                          );
 
+                        case "Not Paid":
+                          return (
+                            <div className="rounded-[8px] bg-[#ffd9dc] mb-3 mt-3  border-[1px] border-[#df2020]  items-center justify-between px-[30px] py-1 cursor-pointer hover:shadow-lg transform hover:scale-[101%] transition duration-300 ease-out">
+                              <div className="flex  justify-between w-full mb-1">
+                                <div className="flex  justify-start gap-2 ">
+                                  <h1 className="mt-1 text-sm">{child.name}</h1>
+                                </div>
+                                <div className="flex  justify-end gap-2 ">
+                                  <h1 className="mt-1 text-[#ff0000]">
+                                    {child.paymentStatus}
+                                  </h1>
+                                </div>
+                              </div>
+                              <div className="text-slate-600 text-xs ">
+                                {child.id}
+                              </div>
+                            </div>
+                          );
+                        default:
+                          break;
+                      }
+                    })()}
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+          {/* end of right column */}
+        </div>
+        {/* end of full box */}
       </MainLayout>
     </div>
   );
