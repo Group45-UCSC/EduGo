646bf9384c58091ede2249e321460c089a76e769
Dewmini Rathnawardhana <108827091+dewminisavindrani@users.noreply.github.com>
2023-08-12 15:07:36 +0530

    update Evaluation criteria

--- client/src/components/Model/VCmodal.js
+++ client/src/components/Model/VCmodal.js
@@ -14,7 +14,7 @@ function VCmodal({visible, onClose}) {
   return (
    
     <div className="fixed inset-0 bg-black bg-opacity-9 backdrop-blur-sm overflow-y-scroll ">
-        <div className="ml-[380px] mt-8 mb-6 pt-4 bg-[#F4F4F4] w-[970px] h-[940px] rounded-lg shadow-lg drop-shadow-lg">
+        <div className="ml-[380px] mt-8 mb-6 pt-4 bg-[#F4F4F4] w-[970px] h-[1220px] rounded-lg shadow-lg drop-shadow-lg">
         {/* topic */}
         <div className='font-bold ml-12 mt-4 text-2xl'>Condition Check</div>
 
@@ -60,11 +60,47 @@ function VCmodal({visible, onClose}) {
         <button className="ml-[400px] p-2 mb-2 mt-3 font-semibold bg-gradient-to-b from-amber-500 to-amber-300 w-52  rounded-lg shadow-md hover:shadow-lg transform hover:scale-[102%] trasition duration-300 ease-out  hover:cursor-pointer" >Next date 2023/09/25</button>
 
         {/* secondsqure */}
-        <div className="mt-4 ml-10 pl-8 pt-4 pb-3 mb-8 bg-[rgb(244,244,244)] w-[900px]  rounded-lg shadow-md drop-shadow-md">
+        <div className="mt-4 ml-10 pl-8 pt-4 pb-3 mb-3 bg-[rgb(244,244,244)] w-[900px] h-[500px] rounded-lg shadow-md drop-shadow-md">
             <h1 className="font-bold p-1 text-[19px]">Feedback Report</h1>
             <form className="">
-                <input type="text" placeholder="Comment on the vehicle condition here..." className="w-[850px] h-[100px] pl-6 pt-1 pb-2 mb-3 rounded-lg shadow-md drop-shadow-md"></input>
-                <button onClick={onClose}  className="ml-[710px] p-2 mb-2 font-semibold bg-gradient-to-b from-amber-500 to-amber-300 w-32  rounded-lg shadow-md hover:shadow-lg transform hover:scale-[102%] trasition duration-300 ease-out  hover:cursor-pointer" >Submit</button>
+                {/* critiria */}
+                <div className="pl-4 mb-3">
+                    <h1 className="font-bold p-1 text-[16px] text-red-500">Evaluation criteria</h1>
+                    <div className="ml-4">
+                        <input type="checkbox" id="critiria1" name="critiria1" value="one"></input>
+                        <label for="critiria1"> Exterior Condition</label><br/>
+                        <input type="checkbox" id="critiria2" name="critiria2" value="two"></input>
+                        <label for="critiria2"> Interior Condition</label><br/>
+                        <input type="checkbox" id="critiria3" name="critiria3" value="three"></input>
+                        <label for="critiria3"> Mechanical Condition</label><br/>
+                        <input type="checkbox" id="critiria4" name="critiria4" value="four"></input>
+                        <label for="critiria4"> Undercarriage and Suspension</label><br/>
+                        <input type="checkbox" id="critiria5" name="critiria5" value="five"></input>
+                        <label for="critiria5">Documentation and Maintenance History</label><br/>
+                        <input type="checkbox" id="critiria6" name="critiria6" value="six"></input>
+                        <label for="critiria6"> Brakes and Steeringn</label><br/>
+                        <input type="checkbox" id="critiria7" name="critiria7" value="seven"></input>
+                        <label for="critiria7"> Transmission and Drivetrain </label><br/>
+                        <input type="checkbox" id="critiria8" name="critiria8" value="eight"></input>
+                        <label for="critiria8"> Electrical and Electronics </label><br/>
+                    </div>
+                </div>
+                    <input type="text" placeholder="Comment on the vehicle condition here..." className="w-[850px] h-[100px] pl-6 pt-1 pb-2 mb-3 rounded-lg shadow-md drop-shadow-md"></input>
+                
+                <div className="grid grid-cols-2 gap-[150px] mt-4">
+                    {/* switchone */}
+                    <div className="grid grid-cols-3 gap-[10px] ml-3">
+                        <h1> <b>Fail</b></h1>
+                        <label for="check" class="-ml-20 w-20 h-10 rounded-full relative bg-gradient-to-b from-amber-500 to-amber-300  shadow-md hover:shadow-lg cursor-pointer">
+                            <input type="checkbox" id="check" class="sr-only peer"></input>
+                            <span className="w-2/5 h-4/5 bg-red-600 absolute rounded-full m-1 peer-checked:bg-green-800 peer-checked:left-10 transition-all duration-200"></span>
+                        </label>
+                        <h1 className="-ml-24"><b>Pass</b></h1>
+                    </div>
+
+                    {/* switchtwo */}
+                    <button onClick={onClose}  className="ml-[110px] p-2 mb-2 font-semibold bg-gradient-to-b from-amber-500 to-amber-300 w-32  rounded-lg shadow-md hover:shadow-lg transform hover:scale-[102%] trasition duration-300 ease-out  hover:cursor-pointer" >Submit</button><br/>
+                </div>
             </form>
         </div>
         <PrReports onClose={handleOnClose} visible={showPrReports}/>
