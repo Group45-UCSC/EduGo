159e43f1197b92c087bf9102ba50c8480e947dad
Sasindu Udayanga <90015129+sasinduudayanga@users.noreply.github.com>
2023-08-18 13:34:46 +0530

    parent edugo feedback done

--- client/src/views/parent/Feedback.js
+++ client/src/views/parent/Feedback.js
@@ -34,9 +34,14 @@ const sideNavBarLinks = [
 function Feedback() {
   const [values, setValues] = useState({
     feedback_msg: "",
+    edugo_feedback_msg: ""
   });
 
-  const { feedback_msg } = values;
+
+
+  const { feedback_msg, edugo_feedback_msg } = values;
+
+
 
   const handleInput = (event) => {
     setValues((prev) => ({
@@ -82,6 +87,44 @@ function Feedback() {
       console.error(err.message);
     }
   };
+
+  //submit edugo feedback
+  const handleSubmit2 = async(event) => {
+    event.preventDefault();
+
+    try {
+      const body = { edugo_feedback_msg, currentValue2 }
+
+      const response = await fetch(
+        `http://localhost:5000/edugo/parent/edugofeedback/add/${userId}`, 
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(body),
+        }
+      );
+      if (response.status === 200) {
+        swal({
+          title: "Your Feedback Submitted!",
+          icon: "success",
+          buttons: {
+            confirm: {
+              className:
+                "bg-orange text-white px-10 py-2 rounded-lg items-center hover:bg-gray ",
+            },
+          },
+        }).then(() => {
+          console.log(response);
+        });
+      } else {
+        console.log(response);
+      }
+      
+    } catch (err) {
+      console.error(err.message)
+    }
+  };
+  //----------Driver rating stars
   const [currentValue, setCurrentValue] = useState(0);
   const [hoverValue, setHoverValue] = useState(undefined);
   const stars = Array(5).fill(0);
@@ -98,101 +141,24 @@ function Feedback() {
     setHoverValue(undefined);
   };
 
-  // function RatingStars({ rating }) {
-  //   const filledStars = Math.floor(rating);
-  //   const partFilledStar = filledStars + 1;
+  //----------Edugo Rating Stars
+  const [currentValue2, setCurrentValue2] = useState(0);
+  const [hoverValue2, setHoverValue2] = useState(undefined);
+  const stars2 = Array(5).fill(0);
 
-  //   const starFilledWidth = (starIndex) => {
-  //     if (starIndex + 1 <= filledStars) {
-  //       return "100%";
-  //     } else if (starIndex + 1 === partFilledStar) {
-  //       console.log();
-  //       return `${Math.floor((rating - filledStars) * 100)}%`;
-  //     } else {
-  //       return "0%";
-  //     }
-  //   };
+  const handleClick2 = (value) => {
+    setCurrentValue2(value);
+  };
 
-  //   return (
-  //     <div className="rating">
-  //       {Array(5)
-  //         .fill(0)
-  //         .map((star, index) => (
-  //           <div className="star" key={index}>
-  //             <div
-  //               className="starFull"
-  //               style={{
-  //                 width: starFilledWidth(index),
-  //               }}
-  //             >
-  //               <BsStarFill className=" text-sm" />
-  //             </div>
-  //             <div className="starEmpty">
-  //               <BsStar className=" text-sm" />
-  //             </div>
-  //           </div>
-  //         ))}
-  //     </div>
-  //   );
-  // }
-  // const reviews = [
-  //   {
-  //     id: 1,
-  //     u_image: require("../../images/user.png"),
-  //     u_name: "K.L Kumarasiri",
-  //     rating: 3,
-  //     review: "Saman is a very responsible and safe driver. My child enjoys the ride every day!",
-  //   },
-  //   {
-  //     id: 2,
-  //     u_image: require("../../images/user.png"),
-  //     u_name: "M.N. Pasindu Yasith",
-  //     rating: 4,
-  //     review: "Saman is fantastic! She always arrives on time, and my kid feels safe and comfortable during the journey.",
-  //   },
-  //   {
-  //     id: 3,
-  //     u_image: require("../../images/user.png"),
-  //     u_name: "M.N. Pasindu Yasith",
-  //     rating: 2,
-  //     review: "Saman is okay, but sometimes he's a little late. Overall, the service is satisfactory.",
-  //   },
-  //   {
-  //     id: 4,
-  //     u_image: require("../../images/user.png"),
-  //     u_name: "M.N. Pasindu Yasith",
-  //     rating: 4,
-  //     review: "Saman is an amazing driver! She is patient, friendly, and goes the extra mile to ensure the children are happy.",
-  //   },
-  //   {
-  //     id: 5,
-  //     u_image: require("../../images/user.png"),
-  //     u_name: "M.N. Pasindu Yasith",
-  //     rating: 1,
-  //     review: "I had a terrible experience with Saman. He drove recklessly and didn't seem to care about the safety of the children. I would not recommend her as a school driver.",
-  //   },
-  //   {
-  //     id: 6,
-  //     u_image: require("../../images/user.png"),
-  //     u_name: "M.N. Pasindu Yasith",
-  //     rating: 3,
-  //     review: "Saman is fantastic! She always arrives on time",
-  //   },
-  //   {
-  //     id: 7,
-  //     u_image: require("../../images/user.png"),
-  //     u_name: "M.N. Pasindu Yasith",
-  //     rating: 2,
-  //     review: "Saman is okay, but sometimes he's a little late. Overall, the service is satisfactory.",
-  //   },
-  //   {
-  //     id: 8,
-  //     u_image: require("../../images/user.png"),
-  //     u_name: "M.N. Pasindu Yasith",
-  //     rating: 4,
-  //     review: "Saman is an amazing driver! He is patient, friendly, and goes the extra mile to ensure the children are happy.",
-  //   },
-  // ];
+  const handleMouseOver2 = (newHoverValue) => {
+    setHoverValue2(newHoverValue);
+  };
+
+  const handleMouseLeave2 = () => {
+    setHoverValue2(undefined);
+  };
+
+
   return (
     <div>
       <MainLayout data={sideNavBarLinks}>
@@ -231,13 +197,13 @@ function Feedback() {
                           />
                         );
                       })}
-                      <p>Selected rating: {currentValue} out of 5 stars</p>
+                      
                     </div>
                   </div>
                 </div>
-                <div className="h-full gap-2 flex flex-col items-center justify-center">
+                <div className="h-full gap-2 flex flex-col items-center ml-16 justify-center">
                   <form onSubmit={handleSubmit1}>
-                    <input
+                    <textarea
                       name="feedback_msg"
                       type="text"
                       onChange={handleInput}
@@ -245,9 +211,9 @@ function Feedback() {
                       placeholder="What's your experience?"
                     />
 
-                    <div className="flex justify-center w-5/6 ">
+                    <div className="flex justify-center w-5/6 mt-3 ">
                       <button
-                        className="flex justify-center w-[350px] h-10 bg-orange rounded-md cursor-pointer hover:shadow-lg transform hover:scale-[103%] transition duration-300 ease-out"
+                        className="flex justify-center w-[420px] h-10 bg-orange rounded-md cursor-pointer hover:shadow-lg transform hover:scale-[103%] transition duration-300 ease-out"
                         type="submit"
                       >
                         <div className="flex mt-2 gap-3 font-semibold">
@@ -261,30 +227,7 @@ function Feedback() {
             </div>
             <div className="  h-[350px] ">
               <div className=" mt-8">
-                {/* <div className=" h-8 mb-3 flex justify-center">
-                  <h1 className="text-xl font-bold ">Driver Reviews</h1>
-                </div>
-                <div className=" px-3 h-[460px] overflow-y-auto">
-                  {reviews.map((review, index) => (
-                    <div key={index} className="rounded-[8px] bg-slate-100 mb-3  border-[1px] border-orange  items-center justify-between px-[30px] py-3 cursor-pointer hover:shadow-lg transform hover:scale-[101%] transition duration-300 ease-out">
-                      <div className="flex  w-full mb-3">
-                        <div className="flex justify-start gap-2 ">
-                          <img src={review.u_image} alt="user_image" className="bg-slate-300 w-8 cursor-pointer rounded-full p-1"></img>
-                          <h1 className="mt-1">{review.u_name}</h1>
 
-                        </div>
-                        <div className="flex justify-end mt-2  ml-auto">
-                          <RatingStars rating={review.rating} />
-                        </div>
-                      </div>
-
-                      <div>
-                        {review.review}
-                      </div>
-
-                    </div>
-                  ))}
-                </div> */}
                 <div className="">
                   <img
                     src={logo_old}
@@ -294,21 +237,28 @@ function Feedback() {
                 </div>
               </div>
             </div>
-            <div className="gid col-span-2 bg-slate-200 rounded-md h-[200px] px-3 py-3 mb-3 ">
+            <div className="gid col-span-2 bg-slate-200 rounded-md h-[220px] px-3 py-3 mb-3">
               {/* --------Rating Stars----------- */}
-              <div className=" h-8">
+
+              <div className=" h-16 ">
+                <h3 className="">“Edugo” How supports for you ?</h3>
+                <h3 className="">
+                  Your valuable feedback will cause to our improvement .{" "}
+                </h3>
+              </div>
+              <div className=" h-8 flex justify-start pl-3">
                 <div style={styles.container}>
                   <div style={styles.stars}>
-                    {stars.map((_, index) => {
+                    {stars2.map((_, index) => {
                       return (
                         <FaStar
                           key={index}
                           size={24}
-                          onClick={() => handleClick(index + 1)}
-                          onMouseOver={() => handleMouseOver(index + 1)}
-                          onMouseLeave={handleMouseLeave}
+                          onClick={() => handleClick2(index + 1)}
+                          onMouseOver={() => handleMouseOver2(index + 1)}
+                          onMouseLeave={handleMouseLeave2}
                           color={
-                            (hoverValue || currentValue) > index
+                            (hoverValue2 || currentValue2) > index
                               ? colors.orange
                               : colors.grey
                           }
@@ -319,28 +269,24 @@ function Feedback() {
                         />
                       );
                     })}
-                    <p>Selected rating: {currentValue} out of 5 stars</p>
+                  
                   </div>
                 </div>
               </div>
-              <div className=" h-16 ">
-                <h3 className="">“Edugo” How supports for you ?</h3>
-                <h3 className="">
-                  Your valuable feedback will cause to our improvement .{" "}
-                </h3>
-              </div>
-              <div className=" h-24 p-2">
-                <div className="p-4 bg-slate-200 border border-gray rounded-lg">
-                  <form className="flex items-center justify-between">
+              <div className=" h-28 p-2">
+                <div className="p-4  bg-slate-200 border border-gray rounded-lg">
+                  <form onSubmit={handleSubmit2} className="flex items-center justify-between">
                     <input
                       type="text"
+                      name="edugo_feedback_msg"
                       className="form-control h-12 w-full  p-1.5 text-base text-gray-700 bg-white border-[1px] border-orange rounded focus:ring focus:ring-blue-300 focus:border-blue-500 transition-all"
                       aria-label="message…"
+                      onChange={handleInput}
                       placeholder="Write message…"
                     />
 
                     <button
-                      type="button"
+                      type="submit"
                       className="w-[150px] h-12 ml-1 bg-orange px-2 py-1.5  rounded"
                     >
                       <div className="flex justify-center gap-4">
--- server/src/routes/parentRoutes.js
+++ server/src/routes/parentRoutes.js
@@ -5,9 +5,11 @@ const router = express.Router();
 //import functions
 const { updateChild } = require("../controllers/parent/childController");
 const { addFeedback } = require("../controllers/parent/feedbackController");
+const { addEdugoFeedback } = require("../controllers/parent/feedbackController");
 
 //create routes
 router.route("/updatechild/:id").put(updateChild);
 router.route("/feedback/add/:userId").post(addFeedback);
+router.route("/edugofeedback/add/:userId").post(addEdugoFeedback);
 
 module.exports = router;
