93b1e816c6553bd6e1b7690e9b69c9553ccafda2
Charitha Bandaraadhikaram <charitharuwindu@gmail.com>
2023-09-10 02:09:04 +0530

    add employee back end

--- server/src/controllers/admin/empController.js
+++ server/src/controllers/admin/empController.js
@@ -5,13 +5,11 @@ const pool = require("../../dbConnection");
 const addEmployee = async (req, res) => {
   try {
     //1. destructure the req.body(name, email, password)
-    const { name, email, tpNum, nic, password, address, dob, role } = req.body;
+    const { name, email, tpNum, nic, password, role } = req.body;
 
     //2. check if user exist(if exist then throw error)
 
-    const user = await pool.query("SELECT * FROM employee WHERE email = $1", [
-      email,
-    ]);
+    const user = await pool.query("SELECT * FROM employee WHERE email =   '" + email + "'");
 
     if (user.rows.length !== 0) {
       return res.status(401).json("User already exists");
@@ -20,8 +18,8 @@ const addEmployee = async (req, res) => {
     //4. enter the new user inside our database
 
     const newUser = await pool.query(
-      "INSERT INTO employee (name,email,tpNum,nic,password,address,dob,role,reg_date) VALUES($1,$2,$3,$4,$5,$6,$7,$8,NOW()) RETURNING * ",
-      [name, email, tpNum, nic, password, address, dob, role]
+      "INSERT INTO employee (name,email,tpNum,nic,password,role,reg_date) VALUES($1,$2,$3,$4,$5,$6,NOW()) RETURNING * ",
+      [name, email, tpNum, nic, password, role]
     );
 
     //5. register success
