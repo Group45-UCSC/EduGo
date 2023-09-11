522e437bcee6518e8351cc93215974017fdeffce
L.L. Apsara Hansani <apsliyanage99@gmail.com>
2023-09-11 16:25:39 +0530

    select school from list frontend implemented

--- client/src/views/driver/Schools.js
+++ client/src/views/driver/Schools.js
@@ -7,8 +7,19 @@ import {
   MdOutlineRateReview,
   MdLocationOn,
 } from "react-icons/md";
+
+import { TfiCheckBox } from "react-icons/tfi";
+
+// import { BiSolidSelectMultiple } from "react-icons/bi";
+import {
+  RiAddBoxLine,
+  RiCheckboxLine,
+  RiCheckboxIndeterminateLine,
+} from "react-icons/ri";
+
 import { useState, useEffect } from "react";
 import swal from "sweetalert";
+
 import { useNavigate, NavLink } from "react-router-dom";
 
 const sideNavBarLinks = [
@@ -26,26 +37,176 @@ const sideNavBarLinks = [
 function Schools() {
   const userId = localStorage.getItem("userId");
 
-  //get total ride details----------------------------------------
+  //get total school details------------------------------------------------------------------------------------------------------------
   const [allSchoolList, setSchoolList] = useState([]);
   const [regSchoolList, setRegSchoolList] = useState([]);
 
-  useEffect(() => {
-    async function getSchoolDetails() {
-      try {
-        const response = await fetch(
-          `http://localhost:5000/edugo/driver/ride/view/school/${userId}`
-        );
-        const data = await response.json();
-        setSchoolList(data.schoolList);
-        setRegSchoolList(data.regSchoolList);
-      } catch (err) {
-        console.error(err.message);
-      }
-    }
+  //   useEffect(() => {
+  //     async function getSchoolDetails() {
+  //       try {
+  //         const response = await fetch(
+  //           `http://localhost:5000/edugo/driver/ride/view/school/${userId}`
+  //         );
+  //         const data = await response.json();
+  //         setSchoolList(data.schoolList);
+  //         setRegSchoolList(data.regSchoolList);
+  //       } catch (err) {
+  //         console.error(err.message);
+  //       }
+  //     }
 
-    getSchoolDetails();
+  //     getSchoolDetails();
+  //   }, [userId]);
+
+  async function getSchoolDetails(setSchoolList, setRegSchoolList, userId) {
+    try {
+      const response = await fetch(
+        `http://localhost:5000/edugo/driver/ride/view/school/${userId}`
+      );
+      const data = await response.json();
+      setSchoolList(data.schoolList);
+      setRegSchoolList(data.regSchoolList);
+    } catch (err) {
+      console.error(err.message);
+    }
+  }
+
+  useEffect(() => {
+    // Call getSchoolDetails from useEffect
+    getSchoolDetails(setSchoolList, setRegSchoolList, userId);
   }, [userId]);
+
+  //use when page must be reloaded with data
+  const recallData = async () => {
+    await getSchoolDetails(setSchoolList, setRegSchoolList, userId);
+  };
+
+  //submit selected new school-----------------------------------------------------------------------------------------------------------
+
+  const handleSubmit = async (schoolId) => {
+    // event.preventDefault();
+
+    try {
+      const body = { schoolId: schoolId };
+      const response = await fetch(
+        `http://localhost:5000/edugo/driver/ride/select/school/${userId}`,
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(body),
+        }
+      );
+      if (response.status === 200) {
+        swal({
+          title: "Selected School is sucessfully added!",
+          icon: "success",
+          buttons: {
+            confirm: {
+              className:
+                "bg-orange text-white px-10 py-2 rounded-lg items-center hover:bg-gray ",
+            },
+          },
+        }).then(() => {
+          // window.location.reload();
+          recallData();
+        });
+      } else {
+        console.log(response);
+      }
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  //handle new school selecting option and alert display-------------------------------------------------------------------------------------------
+
+  const handleAddClick = (schoolId) => {
+    swal({
+      title: "Are you sure?",
+      text: "The selected school will be add to your school ride",
+      icon: "warning",
+      buttons: ["No, cancel!", "Yes, add it!"],
+      dangerMode: true,
+    }).then((confirmed) => {
+      if (confirmed) {
+        handleSubmit(schoolId);
+      } else {
+      }
+    });
+  };
+
+  //remove selected school-----------------------------------------------------------------------------------------------------------
+
+//   const handleRemove = async (schoolId) => {
+//     // event.preventDefault();
+
+//     try {
+//       const body = { schoolId: schoolId };
+//       const response = await fetch(
+//         `http://localhost:5000/edugo/driver/ride/select/school/${userId}`,
+//         {
+//           method: "POST",
+//           headers: { "Content-Type": "application/json" },
+//           body: JSON.stringify(body),
+//         }
+//       );
+//       if (response.status === 200) {
+//         swal({
+//           title: "Selected School is sucessfully added!",
+//           icon: "success",
+//           buttons: {
+//             confirm: {
+//               className:
+//                 "bg-orange text-white px-10 py-2 rounded-lg items-center hover:bg-gray ",
+//             },
+//           },
+//         }).then(() => {
+//           // window.location.reload();
+//           recallData();
+//         });
+//       } else {
+//         console.log(response);
+//       }
+//     } catch (err) {
+//       console.error(err.message);
+//     }
+//   };
+
+  //handle school removing option and alert display-------------------------------------------------------------------------------------------
+
+  const handleRemoveClick = async (schoolId) => {
+    try {
+      const response = await fetch(
+        `http://localhost:5000/edugo/driver/ride/checkBeforeRemove/school/${userId},${schoolId}`
+      );
+      const data = await response.json();
+
+      //check whether related school has childrens in this ride
+      if (data === "can't delete") {
+        handleRemove(schoolId);
+      }
+      //prevent the removing
+      else {
+        swal({
+          title:
+            "You can't remove this school from your ride!\n There are children of this school in your school ride\nPlaease Contact Support agent if need.",
+          icon: "success",
+          buttons: {
+            confirm: {
+              className:
+                "bg-orange text-white px-10 py-2 rounded-lg items-center hover:bg-gray ",
+            },
+          },
+        }).then(() => {
+          // window.location.reload();
+          // recallData();
+        });
+      }
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
   //   const isBothRide =
   //   rideDataList.length > 0 && rideDataList[0].ride_type === "both";
 
@@ -81,19 +242,19 @@ function Schools() {
                 return (
                   <div
                     key={index}
-                    className="z-0 w-[100%] rounded-[8px] bg-slate-200 border-l-[4px] border-orange flex items-center justify-between px-[30px] cursor-pointer hover:shadow-lg transform hover:scale-[103%] transition duration-300 ease-out"
+                    className="z-0 w-[100%] rounded-[8px] bg-slate-200 border-t-[1.4px] border-orange flex items-center justify-between px-[30px] cursor-pointer hover:shadow-lg transform hover:scale-[103%] transition duration-300 ease-out"
                   >
-                    <div className=" px-5 flex gap-1 w-1/3 leading-4">
+                    <div className=" px-5 flex gap-1 w-1/5 leading-4">
                       <div className="text-[12px] font-semibold">
                         {school.school_id}
                       </div>
                     </div>
-                    <div className="flex gap-1 w-1/3 leading-4">
+                    <div className="flex gap-1 w-1/5 leading-4">
                       <div className="text-[12px] font-semibold">
                         {school.school_name}
                       </div>
                     </div>
-                    <div className="flex gap-1 w-1/3 leading-4">
+                    <div className="flex gap-1 w-1/5 leading-4">
                       <button
                         onClick={() => {
                           //   setModalOpen2(true);
@@ -104,41 +265,47 @@ function Schools() {
                           <MdLocationOn className=" text-[20px]" />
                         </div>
                       </button>
+                    </div>
 
-                      {isRegistered ? (
-                        <>
-                          <div className="text-green-500">✔️</div>
+                    {isRegistered ? (
+                      <>
+                        {/* remove button------------------------------------------------------- */}
+                        <div className="flex gap-1 w-1/5 leading-4">
                           <button
-                            onClick={() => {
-                              // Handle remove action here
-                              // Call a function to remove the school from the registered list
-                              // You can pass school.school_id as an argument to this function
-                            }}
+                            onClick={() => handleRemoveClick(school.school_id)}
                             className="flex justify-center m-2 w-8 h-8 bg-red-500 hover:bg-red-700 rounded-md cursor-pointer"
                           >
                             <div className="flex mt-2 gap-3 font-semibold">
-                              Remove
+                              <RiCheckboxIndeterminateLine />
                             </div>
                           </button>
-                        </>
-                      ) : (
-                        <>
-                          <div className="text-blue-500">+</div>
+                        </div>
+
+                        {/* selected button----------------------------------------------- */}
+                        <div className="flex gap-1 w-1/5 leading-4">
+                          <button className="flex justify-center m-2 w-8 h-8 bg-green-300 rounded-md cursor-pointer">
+                            <div className="flex mt-2 gap-3 font-semibold">
+                              <TfiCheckBox />
+                            </div>
+                          </button>
+                        </div>
+                      </>
+                    ) : (
+                      <>
+                        {/* add button---------------------------------------------------------------------- */}
+                        <div className="flex gap-1 w-2/5 leading-4">
                           <button
-                            onClick={() => {
-                              // Handle add action here
-                              // Call a function to add the school to the registered list
-                              // You can pass school.school_id as an argument to this function
-                            }}
+                            onClick={() => handleAddClick(school.school_id)}
                             className="flex justify-center m-2 w-8 h-8 bg-blue-500 hover:bg-blue-700 rounded-md cursor-pointer"
                           >
                             <div className="flex mt-2 gap-3 font-semibold">
-                              Add
+                              <RiAddBoxLine />
                             </div>
                           </button>
-                        </>
-                      )}
-                    </div>
+                        </div>
+                        {/* ----------------------------------------------------------------------------------------- */}
+                      </>
+                    )}
                   </div>
                 );
               })}
