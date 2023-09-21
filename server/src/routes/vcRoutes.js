d2e2f2b202e2f30b296a44d719bcb2ecc9a205ca
Dewmini Rathnawardhana <108827091+dewminisavindrani@users.noreply.github.com>
2023-09-22 01:20:24 +0530

    read ongoing rides

--- server/src/routes/vcRoutes.js
+++ server/src/routes/vcRoutes.js
@@ -6,10 +6,12 @@ const router = express.Router();
 const {vehicleList} = require("../controllers/v_coordinator/vehicleController");
 const {emergencyls} = require("../controllers/v_coordinator/emergencyController");
 const {ridealllist} = require("../controllers/v_coordinator/rideController");
+const {ongoingList} = require("../controllers/v_coordinator/rideController");
 
 //create routes
 router.route("/vehicles/vehicle").get(vehicleList);
 router.route("/emergency/emergencytbl").get(emergencyls);
 router.route("/rides/ridetbl").get(ridealllist);
+router.route("/rides/ongoingtbl").get(ongoingList);
 
 module.exports = router;
