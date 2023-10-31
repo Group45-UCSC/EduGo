6979c7fea50a74d9fdfc0c41b4ba2ee684a55763
Kawya Sandamini <2020cs085@stu.ucsc.cmb.ac.lk>
2023-10-31 16:24:37 +0530

    Insert google map into school ride children

--- zmobile_app/lib/driver_pages/rides.dart
+++ zmobile_app/lib/driver_pages/rides.dart
@@ -1,175 +1,112 @@
-// ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors, prefer_const_literals_to_create_immutables, avoid_unnecessary_containers
+// ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors, prefer_const_literals_to_create_immutables, avoid_unnecessary_containers, avoid_print
+
+import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import './navbar.dart';
 
-class RidePage extends StatelessWidget {
+class RidePage extends StatefulWidget {
+  const RidePage({super.key});
+
+  @override
+  State<RidePage> createState() => _RidePageState();
+}
+
+class _RidePageState extends State<RidePage> {
+  final Completer<GoogleMapController> _controller = Completer();
+
+  static const CameraPosition _initailPosition = CameraPosition(
+    target: LatLng(6.9022172, 79.8612785),
+    zoom: 14,
+  );
+
+  final List<Marker> myMarkers = [];
+  final List<Marker> markerList = const [
+    Marker(
+        markerId: MarkerId('First'),
+        position: LatLng(6.9022172, 79.8612785),
+        infoWindow: InfoWindow(
+          title: "UCSC",
+        )),
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    myMarkers.addAll(markerList);
+    packData();
+  }
+
+  Future<Position> getUserLocation() async {
+    await Geolocator.requestPermission()
+        .then((value) {})
+        .onError((error, stackTrace) {
+      print('error$error');
+    });
+
+    return await Geolocator.getCurrentPosition();
+  }
+
+  packData() {
+    getUserLocation().then((value) async {
+      print("My Location");
+      print('${value.latitude} ${value.longitude}');
+
+      myMarkers.add(
+        Marker(
+          markerId: MarkerId('Second'),
+          position: LatLng(value.latitude, value.longitude),
+          infoWindow: InfoWindow(
+            title: 'My Location',
+          ),
+        ),
+      );
+      CameraPosition cameraPosition = CameraPosition(
+        target: LatLng(value.latitude, value.longitude),
+        zoom: 14,
+      );
+
+      final GoogleMapController controller = await _controller.future;
+
+      controller.animateCamera(CameraUpdate.newCameraPosition(cameraPosition));
+      setState(() {
+
+      });
+    });
+  }
+
+ 
+  Widget buildGoogleMap(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: GoogleMap(
+          initialCameraPosition: _initailPosition,
+          markers: Set<Marker>.of(myMarkers),
+          onMapCreated: (GoogleMapController controller) {
+            _controller.complete(controller);
+          },
+        ),
+      ),
+    );
+  } 
+  
   @override
   Widget build(BuildContext context) {
     return Navbar(
       child: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Text(
-              'Ride Page',
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
-          Expanded(
-            flex: 4,  
-            child: Image.asset('assets/images/ss2.png'),
-          ),
-          Padding(
-            padding: EdgeInsets.all(20.0),
-            child: Text(
-              'Ongoing Rides',
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
-          Expanded(
-            flex: 3,
-            child: Scrollable(
-              axisDirection: AxisDirection.right,
-              viewportBuilder: (context, offset) {
-                return SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  child: DataTable(
-                    columns: const <DataColumn>[
-                      DataColumn(label: Text('Action')),
-                      DataColumn(label: Text('Name')),
-                      DataColumn(label: Text('Destination Address')),
-                    ],
-                    rows: <DataRow>[
-                      DataRow(cells: <DataCell>[
-                        DataCell(Container(
-                          child: Row(
-                            children: [
-                              ElevatedButton(
-                                onPressed: () {},
-                                style: ElevatedButton.styleFrom(
-                                  backgroundColor: Color.fromARGB(255, 11, 199, 58),
-                                ),
-                                child: Text('Pick'),
-                              ),
-                              SizedBox(width: 8),  
-                              ElevatedButton(
-                                onPressed: () {},
-                                style: ElevatedButton.styleFrom(
-                                  backgroundColor: Colors.orange,
-                                ),
-                                child: Text('Drop'),
-                              ),
-                              SizedBox(width: 8),
-                              ElevatedButton(
-                                onPressed: () {},
-                                style: ElevatedButton.styleFrom(
-                                  backgroundColor: Colors.red,
-                                ),
-                                child: Text('Missed'),
-                              ),
-                            ],
-                          ),
-                        )),
-                        DataCell(Text('Nilmi Disanayaka')),
-                        DataCell(Text(
-                          'No. 12, Highway Road, Colombo 05',
-                          maxLines: 1,
-                          overflow: TextOverflow.ellipsis,
-                        )),
-                      ]),
-                      DataRow(cells: <DataCell>[
-                        DataCell(Container(
-                          child: Row(
-                            children: [
-                              ElevatedButton(
-                                onPressed: () {},
-                                style: ElevatedButton.styleFrom(
-                                  backgroundColor: Color.fromARGB(255, 11, 199, 58),
-                                ),
-                                child: Text('Pick'),
-                              ),
-                              SizedBox(width: 8),  
-                              ElevatedButton(
-                                onPressed: () {},
-                                style: ElevatedButton.styleFrom(
-                                  backgroundColor: Colors.orange,
-                                ),
-                                child: Text('Drop'),
-                              ),
-                              SizedBox(width: 8),
-                              ElevatedButton(
-                                onPressed: () {},
-                                style: ElevatedButton.styleFrom(
-                                  backgroundColor: Colors.red,
-                                ),
-                                child: Text('Missed'),
-                              ),
-                            ],
-                          ),
-                        )),
-                      
-                        DataCell(Text('Amanda Perera')),
-                        DataCell(Text(
-                          '12B, wijerama Road, Colombo 07',
-                          maxLines: 1,
-                          overflow: TextOverflow.ellipsis,
-                        )),
-                        
-                      ]),
-                      DataRow(cells: <DataCell>[
-                        DataCell(Container(
-                          child: Row(
-                            children: [
-                              ElevatedButton(
-                                onPressed: () {},
-                                style: ElevatedButton.styleFrom(
-                                  backgroundColor: Color.fromARGB(255, 11, 199, 58),
-                                ),
-                                child: Text('Pick'),
-                              ),
-                              SizedBox(width: 8),                              
-                              ElevatedButton(
-                                onPressed: () {},
-                                style: ElevatedButton.styleFrom(
-                                  backgroundColor: Colors.orange,
-                                ),
-                                child: Text('Drop'),
-                              ),
-                              SizedBox(width: 8),
-                              ElevatedButton(
-                                onPressed: () {},
-                                style: ElevatedButton.styleFrom(
-                                  backgroundColor: Colors.red,
-                                ),
-                                child: Text('Missed'),
-                              ),
-                            ],
-                          ),
-                        )),
-                      
-                        DataCell(Text('Malithi Silva')),
-                        DataCell(Text(
-                          '4B, Kirulapana',
-                          maxLines: 1,
-                          overflow: TextOverflow.ellipsis,
-                        )),
-                        
-                      ]),
-                    ],
-                  ),
-                );
-              },
-            ),
-          ),
-        ],
-      ),
+      children: [
+        Expanded(
+          child: buildGoogleMap(context),
+        ),
+        
+      
+      ],
+    ),
+
     );
+     
+    // 
   }
 }
