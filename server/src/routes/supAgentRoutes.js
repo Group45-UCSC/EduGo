cc9e0c98c4c70eb27f4ce2d98253eecdd5866b9c
Nethmini1999 <nethminidilshani99@gmail.com>
2023-10-31 21:56:44 +0530

    supAgent profile

--- server/src/routes/supAgentRoutes.js
+++ server/src/routes/supAgentRoutes.js
@@ -11,7 +11,12 @@ const {
   viewDriverDetails,
   viewVehicleDetails,
 } = require("../controllers/sup_agent/detailsController");
-const { viewChatItems, sendMessage, receiveMessage } = require("../controllers/sup_agent/chatController");
+const {
+  viewChatItems,
+  sendMessage,
+  receiveMessage,
+} = require("../controllers/sup_agent/chatController");
+const {viewProfileDetails} = require("../controllers/sup_agent/profileController")
 //create routes
 // router.route("/complaints/all").get(viewAllComplaints);
 router.route("/parents/viewParent").get(viewParentDetails);
@@ -21,5 +26,6 @@ router.route("/drivers/viewVehicle").get(viewVehicleDetails);
 router.route("/chat/viewChat").get(viewChatItems);
 router.route("/chat/sendMessage/:userId").post(sendMessage);
 router.route("/chat/receiveMessage/:userId").get(receiveMessage);
+router.route("/supAgentProfile/viewDetails/:userId").get(viewProfileDetails);
 
 module.exports = router;
